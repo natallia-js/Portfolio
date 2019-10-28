88f4df1001fefdc49d4b098e7144f72aa29af3dd
Natallia <55149770+natallia-js@users.noreply.github.com>
2019-10-28 15:23:02 +0300

    feat: completed porfolio main page

--- collapsible.js
+++ collapsible.js
@@ -1,5 +1,5 @@
 function showMyEducation() {
-    var coll = document.getElementsByClassName("content");
+    var coll = document.getElementsByClassName("education-info");
     var content = coll[0];
     if (content.style.maxHeight){
         content.style.maxHeight = null;
