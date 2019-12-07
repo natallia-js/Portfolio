9c60e4107ae77857ece1db81cd0384f9e706db7f
Natallia <55149770+natallia-js@users.noreply.github.com>
2019-12-07 16:28:04 +0300

    Add files via upload

--- collapsible.js
+++ collapsible.js
@@ -3,7 +3,9 @@ function showMyEducation() {
     var content = coll[0];
     if (content.style.maxHeight){
         content.style.maxHeight = null;
+        document.getElementById("show-education-info").innerHTML = "&#9660;";
       } else {
         content.style.maxHeight = content.scrollHeight + "px";
+        document.getElementById("show-education-info").innerHTML = "&#9658;";
       }
 };
